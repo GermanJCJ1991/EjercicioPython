aba2fdbbf18e74a16416c21e61fa53584420e494
German33 <germanjesuscastrojimenez@gmail.com>
2024-12-12 15:29:29 -0600

    Imprimir Valores

---
+++ Ejercicio2.py
@@ -0,0 +1,11 @@
+libros = {
+"1984": {"autor": "George Orwell", "año": 1949, "género": "Distopía",
+"calificación": 4.8},"To Kill a Mockingbird": {"autor": "Harper Lee", "año": 1960, "género":
+"Ficción", "calificación": 4.9},
+"El Gran Gatsby": {"autor": "F. Scott Fitzgerald", "año": 1925, "género":
+"Ficción", "calificación": 4.3}}
+
+print(f"Detalles del libro '{libros}':")
+print(f"Autor: {libros["1984"]['autor']}")
+print(f"Género: {libros["To Kill a Mockingbird"]['género']}")
+print(f"Calificación: {libros["El Gran Gatsby"]['calificación']}")
