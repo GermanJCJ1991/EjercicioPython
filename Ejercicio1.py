be4945d5edbb1db7358e940ccfb780392ef80e0d
German33 <germanjesuscastrojimenez@gmail.com>
2024-12-12 15:19:15 -0600

    Agregue un hola

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,10 +3,11 @@ libros = {
 "calificación": 4.8},"To Kill a Mockingbird": {"autor": "Harper Lee", "año": 1960, "género":
 "Ficción", "calificación": 4.9},
 "El Gran Gatsby": {"autor": "F. Scott Fitzgerald", "año": 1925, "género":
-"Ficción", "calificación": 4.3}
-}
+"Ficción", "calificación": 4.3}}
 
 print(f"Detalles del libro '{libros}':")
 print(f"Autor: {libros["1984"]['autor']}")
 print(f"Género: {libros["To Kill a Mockingbird"]['género']}")
 print(f"Calificación: {libros["El Gran Gatsby"]['calificación']}")
+
+"Hola"
